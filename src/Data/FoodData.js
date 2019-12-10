e8ea7c9c701510dd875b8bd37832a54428078bf6
Sorin Chis <kish.sorin@yahoo.com>
2019-12-10 17:15:31 +0200

    transform food into array and improve Menu Component

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,42 +1,60 @@
-export const foods = [
+export const foodItems = [
   {
     name: "Cheese Pizza",
-    img: "/img/pizza1.jpg"
+    img: "/img/pizza1.jpg",
+    section: "Pizza"
   },
   {
     name: "Peperonii Pizza",
-    img: "/img/pizza2.jpg"
+    img: "/img/pizza2.jpg",
+    section: "Pizza"
   },
   {
     name: "Spice Pizza",
-    img: "/img/pizza3.jpg"
+    img: "/img/pizza3.jpg",
+    section: "Pizza"
   },
   {
     name: "Crampy Burger",
-    img: "/img/pizza4.jpg"
+    img: "/img/pizza4.jpg",
+    section: "Sandwitch"
   },
   {
     name: "King Burger",
-    img: "/img/pizza5.jpg"
+    img: "/img/pizza5.jpg",
+    section: "Sandwitch"
   },
   {
-    name: "Arthur Sandwice",
-    img: "/img/pizza6.jpg"
+    name: "Arthur Sandwitch",
+    img: "/img/pizza6.jpg",
+    section: "Sandwitch"
   },
   {
-    name: "Soloman Hamburger",
-    img: "/img/pizza7.jpg"
+    name: "Solomon Hamburger",
+    img: "/img/pizza7.jpg",
+    section: "Sandwitch"
   },
   {
     name: "Prosciuto Pizza",
-    img: "/img/pizza8.jpg"
+    img: "/img/pizza8.jpg",
+    section: "Pizza"
   },
   {
     name: "Nuggets",
-    img: "/img/nuggets.jpg"
+    img: "/img/nuggets.jpg",
+    section: "Sandwitch"
   },
   {
     name: "Fries",
-    img: "/img/fries.jpg"
+    img: "/img/fries.jpg",
+    section: "Fries"
   }
 ];
+
+export const foods = foodItems.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = [];
+  }
+  res[food.section].push(food);
+  return res;
+}, {});
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -7,18 +7,23 @@ const MenuStyled = styled.div`
   margin: 0px 400px 50px 20px;
 `;
 export default function Menu() {
+  console.log(foods);
   return (
     <MenuStyled>
-      <h1>Menu</h1>
-      <FoodGrid>
-        {foods.map(food => {
-          return (
-            <Food img={food.img}>
-              <FoodLabel>{food.name}</FoodLabel>
-            </Food>
-          );
-        })}
-      </FoodGrid>
+      {Object.entries(foods).map(([sectionName, foods]) => (
+        <>
+          <h1>{sectionName}</h1>
+          <FoodGrid>
+            {foods.map(food => {
+              return (
+                <Food img={food.img} key={food.name}>
+                  <FoodLabel>{food.name}</FoodLabel>
+                </Food>
+              );
+            })}
+          </FoodGrid>
+        </>
+      ))}
     </MenuStyled>
   );
 }
