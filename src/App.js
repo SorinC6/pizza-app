6fbcb4c07676f5da45021974a0b74e1833b93daf
Sorin Chis <kish.sorin@yahoo.com>
2019-12-10 17:25:08 +0200

    move Global Styles in different folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "./Styles/GlobalStyles";
 import Navbar from "./Navbar.js/Navbar";
 import { Banner } from "./Banner/Banner";
 import Menu from "./Menu/Menu";
-
-const GlobalStyle = createGlobalStyle`
-  body{
-    margin:0;
-    font-family: 'Open Sans', sans-serif;
-  }
-  h1,h2,h3{
-    font-family: 'Righteous', cursive;
-  }
-`;
 
 function App() {
   return (
---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    margin:0;
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
