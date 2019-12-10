e1f304dd074e5d81f9212fee85cc48e8a12ce159
Sorin Chis <kish.sorin@yahoo.com>
2019-12-10 17:04:07 +0200

    added more food data

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -10,5 +10,33 @@ export const foods = [
   {
     name: "Spice Pizza",
     img: "/img/pizza3.jpg"
+  },
+  {
+    name: "Crampy Burger",
+    img: "/img/pizza4.jpg"
+  },
+  {
+    name: "King Burger",
+    img: "/img/pizza5.jpg"
+  },
+  {
+    name: "Arthur Sandwice",
+    img: "/img/pizza6.jpg"
+  },
+  {
+    name: "Soloman Hamburger",
+    img: "/img/pizza7.jpg"
+  },
+  {
+    name: "Prosciuto Pizza",
+    img: "/img/pizza8.jpg"
+  },
+  {
+    name: "Nuggets",
+    img: "/img/nuggets.jpg"
+  },
+  {
+    name: "Fries",
+    img: "/img/fries.jpg"
   }
 ];
