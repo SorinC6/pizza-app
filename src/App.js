605401e87101909c8ad22d356195ffd5bec283e7
Sorin Chis <kish.sorin@yahoo.com>
2019-12-10 14:54:07 +0200

    add google fonts, install styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
+      <h1>Pizza App </h1>
       <p>Hello</p>
-    </div>
+    </>
   );
 }
 
