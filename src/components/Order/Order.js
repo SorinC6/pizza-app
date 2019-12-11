483552facf911c72f233ab95dda7c8aae145b5cb
Sorin Chis <kish.sorin@yahoo.com>
2019-12-11 10:59:35 +0200

    added list of orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -20,13 +20,28 @@ const OrderContent = styled(DialogContent)`
   height: 100%;
 `;
 
+const OrderContainer = styled.div`
+  padding: 10px 0;
+  border-bottom: 1px solid grey;
+`;
+const OrderItem = styled.div`
+  padding: 10px 0;
+`;
+
 export default function Order({ orders }) {
   return (
     <OrderStyled>
       {orders.length === 0 ? (
         <OrderContent>Your Order is empty</OrderContent>
       ) : (
-        <OrderContent>Found {orders.length} orders</OrderContent>
+        <OrderContent>
+          <OrderContainer>Your Order</OrderContainer>
+          {orders.map(order => (
+            <OrderContainer>
+              <OrderItem>{order.name}</OrderItem>
+            </OrderContainer>
+          ))}
+        </OrderContent>
       )}
       <DialogFooter>
         <ConfirmButton>Check Out</ConfirmButton>
