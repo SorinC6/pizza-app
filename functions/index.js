0db2e40bc46737935433d8619c60eda6decee1d8
Sorin Chis <kish.sorin@yahoo.com>
2019-12-12 17:33:17 +0200

    added firebase function for sending emails

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const htmlToText = require("nodemailer-html-to-text").htmlToText;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const { email, password } = require("./config");
+
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+mailTransport.use("compile", htmlToText());
+
+const APP_NAME = "Woop Code";
+
+exports.sendUserEmail = functions.database
+  .ref("/orders/{pushId}")
+  .onCreate(order => {
+    sendOrderEmail(order.val());
+  });
+
+function sendOrderEmail(order) {
+  console.log(order);
+  const mailOptions = {
+    from: `${APP_NAME} <sorin.chis06@gmail.com.com`,
+    to: order.email,
+    subject: `Your order from ${APP_NAME}.`,
+    html: `
+      <table style="width:500px; margin: auto"> 
+        <tr>
+            <th>${order.displayName}</th>
+            <th>You ordered some food, here's confirmation of that order. </th>
+        </tr>
+       
+      </table>
+    `
+  };
+  return mailTransport
+    .sendMail(mailOptions)
+    .then(info => {
+      console.log("Email Sent: ", +info.response);
+    })
+    .catch(error => {
+      console.log("Error sending email: ", error);
+    });
+}
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -92,7 +92,6 @@ export default function Order({
         };
       }, {});
     });
-    console.log(newOrders);
 
     newOrderRef.set({
       orders: newOrders,
